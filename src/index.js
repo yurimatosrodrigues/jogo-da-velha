18e2dfe60d9edafdd67184d4c0728bb9b4c600f5
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2022-11-12 19:10:31 -0300

    Criação da estrutura e teste da props

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+class Square extends React.Component{
+  render(){
+      return(
+          <button className="square">
+              {this.props.value}
+          </button>
+      );
+  }
+}
+
+class Board extends React.Component{
+  renderSquare(i){
+      return <Square value={i}/>;
+  }
+
+  render(){
+      const status = 'Next player: X';
+      return (
+          <div>
+              <div className="status"> {status} </div>
+              <div className="board-row">
+                  {this.renderSquare(0)}
+                  {this.renderSquare(1)}
+                  {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+              </div>
+          </div>
+      );
+  }
+}
+
+class Game extends React.Component{
+  render() {
+      return(
+          <div className="game">
+              <div className="game-board">
+                  <Board />
+              </div>            
+              <div className="game-info">
+                  <div>{/**/}</div>
+                  <ol>{/**/}</ol>
+              </div>
+          </div>
+      );
+  }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Game />);
