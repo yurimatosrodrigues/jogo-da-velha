fc78ff52de9d50858068d425ce7ad0534fdfd497
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2022-11-12 21:05:31 -0300

    Guardando estado no tabuleiro e passando function prop para o quadrado

--- src/index.js
+++ src/index.js
@@ -3,43 +3,64 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-class Square extends React.Component{
+class Square extends React.Component{   
   render(){
       return(
-          <button className="square">
-              {this.props.value}
+          <button 
+            className="square" 
+            onClick={()=>{ this.props.onClick(); }}
+           >                
+            {this.props.value}
           </button>
       );
   }
 }
 
 class Board extends React.Component{
-  renderSquare(i){
-      return <Square value={i}/>;
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
 
-  render(){
-      const status = 'Next player: X';
-      return (
-          <div>
-              <div className="status"> {status} </div>
-              <div className="board-row">
-                  {this.renderSquare(0)}
-                  {this.renderSquare(1)}
-                  {this.renderSquare(2)}
-              </div>
-              <div className="board-row">
-                  {this.renderSquare(3)}
-                  {this.renderSquare(4)}
-                  {this.renderSquare(5)}
-              </div>
-              <div className="board-row">
-                  {this.renderSquare(6)}
-                  {this.renderSquare(7)}
-                  {this.renderSquare(8)}
-              </div>
-          </div>
-      );
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i){
+        return(
+            <Square 
+                value={this.state.squares[i]}
+                onClick= { () => this.handleClick(i) }
+            />
+        ); 
+    }
+
+    render(){
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status"> {status} </div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
   }
 }
 
